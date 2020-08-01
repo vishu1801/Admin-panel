2f0a61d8b0d933d4850b813cf7dd30c39bd9f9fe
Vishesh <guptavishesh30@gmail.com>
2020-08-02 00:50:17 +0530

    student teacher commit

---
+++ php/view_student.php
@@ -0,0 +1,80 @@
+<?php
+    include 'connection.php';
+    $conn=OpenCon();
+    extract($_POST);
+    if(isset($_POST['classs'])){
+        $classs=$_POST['classs'];
+        if($classs!="--CLASSES--"){
+            $data =  '<table class="table table-hover">
+                            <tr>
+                                <th>S.No</th>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Class</th>
+                                <th>Address</th>
+                                <th>Mobile</th>
+                                <th>Modify</th>
+                            </tr>';
+            $result=mysqli_query($conn,"SELECT *FROM studentlist WHERE class='$classs' ");
+            if(mysqli_num_rows($result)>0){
+                $count=0;
+                while($rows=mysqli_fetch_assoc($result)){
+                    $data .='<tr>
+                    <td>'.++$count.'</td>
+                    <td>'.$rows['firstname'].'</td>
+                    <td>'.$rows['lastname'].'</td>
+                    <td>'.$rows['class'].'</td>
+                    <td>'.$rows['address'].'</td>
+                    <td>'.$rows['phone'].'</td>
+                    <td><button class="btn btn-success" onclick="editrecord('.$rows['id'].')">Edit</button>
+                    <button class="btn btn-danger" onclick="deleterecord('.$rows['id'].')">Delete</button></td>
+                    </tr>';
+                }
+            }
+            $data .='</table>';
+            echo $data;
+        }
+        else{
+            echo "<script>alert('select class');</script>";
+        }
+    }
+
+    if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['classs']) && isset($_POST['address']) && isset($_POST['phone'])){
+        $result=mysqli_query($conn,"INSERT INTO `studentlist`(`firstname`, `lastname`, `address`, `phone`, `class`) VALUES ( '$firstname' , '$lastname' , '$address' , '$phone' , '$classs')");
+    }
+
+    if(isset($_POST['deleteid'])){
+        $result=mysqli_query($conn,"DELETE FROM `studentlist` WHERE id =$deleteid");
+    }
+
+    if(isset($_POST['editid']) && isset($_POST['editid'])!=""){
+        $response=array();
+        $result=mysqli_query($conn,"SELECT *FROM `studentlist` WHERE id='$editid' ");
+        if(mysqli_num_rows($result)>0){
+            while($rows=mysqli_fetch_assoc($result)){
+                $response=$rows;
+            }
+        }
+        else{
+            $response['status']=200;
+            $response['message']="Data not Found";
+        }
+
+        echo json_encode($response);
+    }else{
+        $response['status']=200;
+        $response['message']="Invalid Id";
+    }
+
+    if(isset($_POST['updatekey'])){
+        $updatekey=$_POST['updatekey'];
+        $updatedphone=$_POST['updatedphone'];
+        $updatedaddress=$_POST['updatedaddress'];
+        $updatedclasss=$_POST['updatedclasss'];
+        $updatedlastname=$_POST['updatedlastname'];
+        $updatedfirstname=$_POST['updatedfirstname'];
+        $result=mysqli_query($conn,"UPDATE `studentlist` SET `firstname`='$updatedfirstname',`lastname`='$updatedlastname',`address`='$updatedaddress',`phone`='$updatedphone',`class`='$updatedclasss' WHERE id='$updatekey' ");
+        
+    }
+
+?>
---
+++ php/view_teacher.php
@@ -0,0 +1,74 @@
+<?php
+    include 'connection.php';
+    $conn=OpenCon();
+    extract($_POST);
+    if(isset($_POST['read'])){
+        $data =  '<table class="table table-hover">
+                        <tr>
+                            <th>S.No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                            <th>Mobile</th>
+                            <th>Modify</th>
+                        </tr>';
+        $result=mysqli_query($conn,"SELECT *FROM teachers");
+        if(mysqli_num_rows($result)>0){
+            $count=0;
+            while($rows=mysqli_fetch_assoc($result)){
+                $data .='<tr>
+                <td>'.++$count.'</td>
+                <td>'.$rows['name'].'</td>
+                <td>'.$rows['email'].'</td>
+                <td>'.$rows['username'].'</td>
+                <td>'.$rows['password'].'</td>
+                <td>'.$rows['mobile'].'</td>
+                <td><button class="btn btn-success" onclick="editrecord('.$rows['id'].')">Edit</button>
+                <button class="btn btn-danger" onclick="deleterecord('.$rows['id'].')">Delete</button></td>
+                </tr>';
+            }
+        }
+        $data .='</table>';
+        echo $data;
+    }
+
+    if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['mobile'])){
+        $result=mysqli_query($conn,"INSERT INTO `teachers`(`name`, `email`, `username`, `password`, `mobile`) VALUES ( '$name' , '$email' , '$username' , '$password' , '$mobile')");
+    }
+
+    if(isset($_POST['deleteid'])){
+        $result=mysqli_query($conn,"DELETE FROM teachers WHERE id =$deleteid");
+    }
+
+    if(isset($_POST['editid']) && isset($_POST['editid'])!=""){
+        $response=array();
+        $result=mysqli_query($conn,"SELECT *FROM teachers WHERE id='$editid' ");
+        if(mysqli_num_rows($result)>0){
+            while($rows=mysqli_fetch_assoc($result)){
+                $response=$rows;
+            }
+        }
+        else{
+            $response['status']=200;
+            $response['message']="Data not Found";
+        }
+
+        echo json_encode($response);
+    }else{
+        $response['status']=200;
+        $response['message']="Invalid Id";
+    }
+
+    if(isset($_POST['updatekey'])){
+        $updatekey=$_POST['updatekey'];
+        $updatedmobile=$_POST['updatedmobile'];
+        $updatedpassword=$_POST['updatedpassword'];
+        $updatedusername=$_POST['updatedusername'];
+        $updatedemail=$_POST['updatedemail'];
+        $updatedname=$_POST['updatedname'];
+        $result=mysqli_query($conn,"UPDATE `teachers` SET `name`='$updatedname',`email`='$updatedemail',`username`='$updatedusername',`password`='$updatedpassword',`mobile`='$updatedmobile' WHERE id='$updatekey' ");
+        
+    }
+
+?>
