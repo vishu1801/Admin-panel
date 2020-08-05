bfe74b34b0aadbd5888c7a0436707a4c04400f8d
Vishesh <guptavishesh30@gmail.com>
2020-08-05 08:26:38 +0530

    fully ajax

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
 function OpenCon(){
     $host_name = "localhost";
-    $user_name = "vishucs";
-    $user_pass = "vishu8979";
+    $user_name = "";
+    $user_pass = "";
     $db_name = "attendancesystem";
     $conn = mysqli_connect($host_name,$user_name,$user_pass,$db_name) or die("connection failed: %s\n".$conn->error);
 
